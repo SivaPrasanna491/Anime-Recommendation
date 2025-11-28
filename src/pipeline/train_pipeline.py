f3386932380b9bff06f4ac89ab16ff04e2c031c3
Siva Prasanna <shivachatti66@gmail.com>
2025-11-28 23:40:45 +0530

    dataset uploaded to drive

--- src/pipeline/train_pipeline.py
+++ src/pipeline/train_pipeline.py
@@ -5,7 +5,7 @@ from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 if __name__=="__main__":
     ingestionObj = DataIngestion()
-    train_path = ingestionObj.initialize_data_ingestion(r'notebooks\Final Data.csv')
+    train_path = ingestionObj.initialize_data_ingestion(f"https://drive.google.com/uc?id=1uIKjoBdBOaSaASPXAmGA0qT7EUPzOTLb&export=download&confirm=t")
     
     transformationObj = DataTransformation()
     train_arr, file_path = transformationObj.initiate_data_transformation(train_path=train_path)
